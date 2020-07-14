9fb9482a1605eb87686a14491578a8a53f24733c
zx64 <zx64@users.noreply.github.com>
2020-07-14 13:41:02 +0100

    Fixed order of icewerx leds. Also fixed active low.

--- 2leds.v
+++ 2leds.v
@@ -1,6 +1,6 @@
 `ifdef ICEWERX
 `define O_2LEDS output o_led_red, output o_led_green,
-`define W_2LEDS wire o_2LEDA, o_2LEDB; assign {o_led_red, o_led_green} = {o_2LEDA, o_2LEDB};
+`define W_2LEDS wire o_2LEDA, o_2LEDB; assign {o_led_green, o_led_red} = {~o_2LEDA, ~o_2LEDB};
 `else
 `define O_2LEDS output [3:0] o_led_x, output [7:0] o_led_y,
 `define W_2LEDS wire o_2LEDA, o_2LEDB; assign o_led_x = 4'b0111; assign o_led_y = {~o_2LEDA, ~o_2LEDB, 6'b111111};
