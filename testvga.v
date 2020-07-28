f158915f2b0792885c98b8457f68be1ef99e22bc
zx64 <zx64@users.noreply.github.com>
2020-07-28 16:10:36 +0100

    More resolutions, uniform vertical colour test bars.

--- testvga.v
+++ testvga.v
@@ -2,6 +2,78 @@
 //`define ICEWERX
 `include "2leds.v"
 
+`define VGA1280
+
+`ifdef VGA640
+// 25.125MHz
+`define CLKF 66
+`define CLKQ 5
+`define HPIX 640
+`define HFP  16
+`define HSP  96
+`define HBP  48
+`define VPIX 480
+`define VFP  10
+`define VSP  2
+`define VBP  33
+`endif
+`ifdef VGA800
+// 39.75MHz
+`define CLKF 52
+`define CLKQ 4
+`define HPIX 800
+`define HFP  40
+`define HSP  128
+`define HBP  88
+`define VPIX 600
+`define VFP  1
+`define VSP  4
+`define VBP  23
+`endif
+`ifdef VGA1024
+// 65.25MHz
+`define CLKF 86
+`define CLKQ 4
+`define HPIX 1024
+`define HFP  24
+`define HSP  136
+`define HBP  160
+`define VPIX 768
+`define VFP  3
+`define VSP  6
+`define VBP  29
+`endif
+`ifdef VGA1152
+// 81MHz
+`define CLKF 53
+`define CLKQ 3
+`define HPIX 1152
+`define HFP  64
+`define HSP  120
+`define HBP  184
+`define VPIX 864
+`define VFP  1
+`define VSP  3
+`define VBP  27
+`endif
+`ifdef VGA1280
+// 108MHz
+`define CLKF 71
+`define CLKQ 3
+`define HPIX 1280
+`define HFP  48
+`define HSP  112
+`define HBP  248
+`define VPIX 1024
+`define VFP  1
+`define VSP  3
+`define VBP  38
+`endif
+
+`ifndef HPIX
+`error No resolution set
+`endif
+
 module top(
     input wire i_clock,
 `ifdef ICEWERX
@@ -22,8 +94,8 @@ module top(
     SB_PLL40_CORE #(
         .FEEDBACK_PATH("SIMPLE"),
         .DIVR(0),
-        .DIVF(52),
-        .DIVQ(4),
+        .DIVF(`CLKF),
+        .DIVQ(`CLKQ),
         .FILTER_RANGE(1)
     ) pll (
 `ifdef ICEWERX
@@ -37,17 +109,20 @@ module top(
         .LOCK(i_pixlocked)
     );
 
-    parameter HOR_FRONT_PORCH = 799;
-    parameter HOR_SYNC_PULSE = HOR_FRONT_PORCH + 40;
-    parameter HOR_BACK_PORCH = HOR_SYNC_PULSE + 128;
-    parameter HOR_MAX = HOR_BACK_PORCH + 88;
+    parameter HOR_FRONT_PORCH = `HPIX;
+    parameter HOR_SYNC_PULSE = HOR_FRONT_PORCH + `HFP;
+    parameter HOR_BACK_PORCH = HOR_SYNC_PULSE + `HSP;
+    parameter HOR_MAX = HOR_BACK_PORCH + `HBP;
     reg [11:0] o_hsync_counter = 0;
 
-    parameter VER_FRONT_PORCH = 599;
-    parameter VER_SYNC_PULSE = VER_FRONT_PORCH + 1;
-    parameter VER_BACK_PORCH = VER_SYNC_PULSE + 4;
-    parameter VER_MAX = 627;
+    parameter VER_FRONT_PORCH = `VPIX;
+    parameter VER_SYNC_PULSE = VER_FRONT_PORCH + `VFP;
+    parameter VER_BACK_PORCH = VER_SYNC_PULSE + `VSP;
+    parameter VER_MAX = VER_BACK_PORCH + `VBP;
     reg [11:0] o_vsync_counter = 0;
+
+
+    parameter HBAR = `HPIX / 6;
 
     reg [4:0] o_flipframe;
 
@@ -57,9 +132,9 @@ module top(
     assign o_vsync = ~(o_vsync_counter >= VER_SYNC_PULSE && o_vsync_counter < VER_BACK_PORCH);
     wire o_enable = (o_hsync_counter <= HOR_FRONT_PORCH) && (o_vsync_counter <= VER_FRONT_PORCH);
 
-    assign o_red = o_enable & ((o_hsync_counter < 200) || (o_hsync_counter > 650));
-    assign o_green = o_enable & (o_hsync_counter >= 150) && (o_hsync_counter < 400);
-    assign o_blue = o_enable & (o_hsync_counter >= 350) && (o_vsync_counter[2]);
+    assign o_red   = o_enable & ((o_hsync_counter < (2 * HBAR)) || (o_hsync_counter > (5 * HBAR)));
+    assign o_green = o_enable & (o_hsync_counter < (4 * HBAR)) && (o_hsync_counter > (1 * HBAR));
+    assign o_blue  = o_enable & (o_hsync_counter > (3 * HBAR));
 
     always @ (posedge i_pixclock)
     begin
