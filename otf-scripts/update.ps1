f4106559d90e532f8022da45ea6c6ceaf7c35389
zx64 <zx64@users.noreply.github.com>
2020-08-11 16:08:06 +0100

    Check the contents of VERSION (if any) before proceeding.

--- otf-scripts/update.ps1
+++ otf-scripts/update.ps1
@@ -3,6 +3,14 @@ $OTFVer = "nightly-$OTFDate"
 $OTFArchive = "fpga-toolchain-windows_amd64-$OTFVer.7z"
 $OTFURL = "https://github.com/open-tool-forge/fpga-toolchain/releases/download/$OTFVer/$OTFArchive"
 
+if (Test-Path .\fpga-toolchain\VERSION) {
+    $InstalledVer = Get-Content .\fpga-toolchain\VERSION
+    if ($OTFVer -eq $InstalledVer) {
+        Write-Host Already at version $OTFVer
+        Exit 0
+    }
+}
+
 Write-Progress "Downloading $OTFArchive..."
 curl.exe -LO $OTFURL
 if (!$?) {
@@ -28,3 +36,5 @@ if (Test-Path fpga-toolchain.old) {
 }
 
 Remove-Item $OTFArchive
+
+Write-Host Finished updating to $OTFVer
