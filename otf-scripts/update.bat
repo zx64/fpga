f0f53794b776ab41fde89de281ac160ab2c6c8f0
zx64 <zx64@users.noreply.github.com>
2020-08-11 16:00:34 +0100

    Check 7z.exe and curl.exe are available.

--- otf-scripts/update.bat
+++ otf-scripts/update.bat
@@ -1,5 +1,17 @@
 @echo off
 setlocal
+call :check 7z.exe || goto error
+call :check curl.exe || goto error
 cd /d %~dp0
 powershell -NonInteractive -NoLogo -NoProfile .\update.ps1
 pause
+exit /b 0
+
+:check
+where /Q %1
+if ERRORLEVEL 1 echo Could not find %1
+goto :eof
+
+:error
+pause
+exit /b 1
