75aaa642dcc3119682124abb1c4fba30c657d5aa
zx64 <zx64@users.noreply.github.com>
2020-09-04 22:38:55 +0100

    Use new Powershell.

--- otf-scripts/update.bat
+++ otf-scripts/update.bat
@@ -1,9 +1,10 @@
 @echo off
 setlocal
+call :check pwsh.exe || goto error
 call :check 7z.exe || goto error
 call :check curl.exe || goto error
 cd /d %~dp0
-powershell -NonInteractive -NoLogo -NoProfile .\update.ps1
+pwsh.exe -NonInteractive -NoLogo -NoProfile .\update.ps1
 pause
 exit /b 0
 
