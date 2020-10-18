e2b2ba70efab0756ea9c40b76c106c5bccff0298
zx64 <zx64@users.noreply.github.com>
2020-10-18 19:40:33 +0100

    Don't do copy glob, just rename fujprog in source dir instead.

--- otf-scripts/update-fpga.bat
+++ otf-scripts/update-fpga.bat
@@ -5,7 +5,7 @@ call :check 7z.exe || goto error
 call :check curl.exe || goto error
 cd /d %~dp0
 pwsh.exe -NonInteractive -NoLogo -NoProfile .\update-fpga.ps1
-copy /y fujprog*.exe fpga-toolchain\bin\fujprog.exe
+copy /y fujprog.exe fpga-toolchain\bin\fujprog.exe
 exit /b 0
 
 :check
